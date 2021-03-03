7acb725a5731f4b28bef0a8713b87a521bddb128
Cardlo45 <unconfigured@null.spigotmc.org>
2021-03-03 16:27:51 +0100

    "New Item" Item added

---
+++ src/main/java/carslo/hk/mcmod/test/item/NewItemItem.java
@@ -0,0 +1,74 @@
+
+package carslo.hk.mcmod.test.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import java.util.List;
+
+import carslo.hk.mcmod.test.Hk400testModElements;
+
+@Hk400testModElements.ModElement.Tag
+public class NewItemItem extends Hk400testModElements.ModElement {
+	@ObjectHolder("hk400test:new_item")
+	public static final Item block = null;
+	public NewItemItem(Hk400testModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(1).rarity(Rarity.EPIC));
+			setRegistryName("new_item");
+		}
+
+		@Override
+		public boolean hasContainerItem() {
+			return true;
+		}
+
+		@Override
+		public ItemStack getContainerItem(ItemStack itemstack) {
+			return new ItemStack(this);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("a New Item"));
+		}
+	}
+}
