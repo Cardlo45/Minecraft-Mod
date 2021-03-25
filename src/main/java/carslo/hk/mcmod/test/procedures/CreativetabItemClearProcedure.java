fcfb02d03b2d7210438a7c98370d8a51d56530f6
Cardlo45 <unconfigured@null.spigotmc.org>
2021-03-25 18:03:18 +0100

    - edited creative-tab item - automaticly removing creative tab item when in inventory

---
+++ src/main/java/carslo/hk/mcmod/test/procedures/CreativetabItemClearProcedure.java
@@ -0,0 +1,30 @@
+package carslo.hk.mcmod.test.procedures;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import carslo.hk.mcmod.test.item.CreativetabItem;
+import carslo.hk.mcmod.test.Hk400testModElements;
+
+@Hk400testModElements.ModElement.Tag
+public class CreativetabItemClearProcedure extends Hk400testModElements.ModElement {
+	public CreativetabItemClearProcedure(Hk400testModElements instance) {
+		super(instance, 13);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure CreativetabItemClear!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof PlayerEntity) {
+			ItemStack _stktoremove = new ItemStack(CreativetabItem.block, (int) (1));
+			((PlayerEntity) entity).inventory.clearMatchingItems(p -> _stktoremove.getItem() == p.getItem(), (int) 999);
+		}
+	}
+}
